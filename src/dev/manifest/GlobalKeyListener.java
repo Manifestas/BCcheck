d649011d4b17a1c35886c6d96469465cac678647
tv.unimol <doslovno@gmail.com>
2018-06-01 12:59:17 +0300

    Added ESC as a condition for unregister nativehook

--- src/dev/manifest/GlobalKeyListener.java
+++ src/dev/manifest/GlobalKeyListener.java
@@ -1,21 +1,32 @@
 package dev.manifest;
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
 public class GlobalKeyListener implements NativeKeyListener {
+
     @Override
-    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
+    public void nativeKeyTyped(NativeKeyEvent keyEvent) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(keyEvent.getKeyCode()));
+
+        if (keyEvent.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent keyEvent) {
 
     }
 
     @Override
-    public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
-
-    }
-
-    @Override
-    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+    public void nativeKeyReleased(NativeKeyEvent keyEvent) {
 
     }
 }
