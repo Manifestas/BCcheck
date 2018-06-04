2f997f262b8061badd87da7025ecdeda83533211
manifest <doslovno@gmail.com>
2018-06-04 22:52:29 +0300

    Added TableModel

---
+++ src/dev/manifest/table/TableModel.java
@@ -0,0 +1,20 @@
+package dev.manifest.table;
+
+import javax.swing.table.AbstractTableModel;
+
+public class TableModel extends AbstractTableModel {
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        return null;
+    }
+}
