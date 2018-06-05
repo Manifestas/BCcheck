17085a7fac68f604535134112593c08f05b60c05
manifest <doslovno@gmail.com>
2018-06-06 00:16:40 +0300

    Added MidiPlayer class

---
+++ src/main/java/dev/manifest/util/MidiPlayer.java
@@ -0,0 +1,39 @@
+package dev.manifest.util;
+
+import javax.sound.midi.*;
+
+
+public class MidiPlayer {
+
+    public static void playNewProduct(int instrument, int note) {
+
+        try {
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, 30, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 50, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, 50, 100);
+            MidiEvent noteOff = new MidiEvent(b, 12);
+            track.add(noteOff);
+
+            player.setSequence(seq);
+            player.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
