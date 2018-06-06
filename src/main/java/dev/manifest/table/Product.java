5ff6d2c3c5799a5bbac413b9615ad18d710f8095
tv.unimol <doslovno@gmail.com>
2018-06-06 12:10:40 +0300

    Added equals() to Product

--- src/main/java/dev/manifest/table/Product.java
+++ src/main/java/dev/manifest/table/Product.java
@@ -11,8 +11,18 @@ public class Product {
         this.color = color;
         this.size = size;
     }
+
     public Product(String name, String color) {
         this(name, color, "");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Product)) {
+            return false;
+        }
+        Product product = (Product) obj;
+        return product.name.equals(this.name) && product.color.equals(this.color) && product.size.equals(this.size);
     }
 
     public String getName() {
