223801bf5c215ffa51a6076ca05232191c3ca3fa
tv.unimol <doslovno@gmail.com>
2018-06-10 17:54:42 +0300

    Adding logging to DbHelper

--- src/main/java/dev/manifest/bccheck/data/DbHelper.java
+++ src/main/java/dev/manifest/bccheck/data/DbHelper.java
@@ -1,5 +1,6 @@
 package dev.manifest.bccheck.data;
 
+import com.sun.istack.internal.NotNull;
 import dev.manifest.bccheck.table.Product;
 import dev.manifest.bccheck.data.DbContract.ModelEntry;
 import dev.manifest.bccheck.data.DbContract.ColorEntry;
@@ -19,7 +20,7 @@ public class DbHelper {
     private static Statement statement;
     private static ResultSet resultSet;
 
-    public static Connection getConnection() throws Exception {
+    private static Connection getConnection() throws Exception {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         connection = DriverManager.getConnection(DbContract.DB_CONN_URL);
 
@@ -28,21 +29,21 @@ public class DbHelper {
         return connection;
     }
 
-    public static void closeConnection() throws SQLException {
+    private static void closeConnection() throws SQLException {
         if (connection != null) {
             connection.close();
             log.finest("Closing connection");
         }
     }
 
-    public static void closeStatement() throws SQLException {
+    private static void closeStatement() throws SQLException {
         if (statement != null) {
             statement.close();
             log.finest("Closing statement");
         }
     }
 
-    public static void closeResultSet() throws SQLException {
+    private static void closeResultSet() throws SQLException {
         if (resultSet != null) {
             resultSet.close();
             log.finest("Closing resultSet");
@@ -59,46 +60,66 @@ public class DbHelper {
         }
     }
 
-    public static ResultSet getResultSet(String barcode) throws SQLException{
-        if (connection != null) {
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(DbContract.queryQty(barcode));
-            return resultSet;
-        } else {
-            return null;
-        }
+    private static ResultSet getResultSet(String barcode) throws SQLException{
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(DbContract.queryQty(barcode));
+        return resultSet;
     }
 
-    public static Product getProductFromResultSet(ResultSet resultSet) throws SQLException {
+    private static Product getProductFromResultSet(ResultSet resultSet) throws SQLException {
         String modelName = resultSet.getString(ModelEntry.COLUMN_MODEL);
         String color = resultSet.getString(ColorEntry.COLUMN_COLOR);
         String size = resultSet.getString(SizeEntry.COLUMN_SIZE_NAME);
 
-        return new Product(modelName, color, size);
+        Product currentProduct = new Product(modelName, color, size);
+
+        log.info("Getting a Product from ResultSet: " + currentProduct);
+
+        return currentProduct;
     }
 
-    public static Product returnProductIfNew(String barcode) {
+    /**
+     * Returns the product received from the database with this barcode,
+     * if there are no any sizes of this article on the residuals
+     * @param barcode of product.
+     * @return A Product with this barcode, if quantity of any of its sizes is zero.
+     */
+    public static Product returnProductIfNew(@NotNull String barcode) {
         Product product = null;
         try {
             if (connection == null) {
+                log.finest("Connection == null");
+
                 connection = getConnection();
             }
             ResultSet rs = getResultSet(barcode);
             if (rs == null) {
+                log.finest("ResultSet == null");
+
                 return null;
             }
+            // check each size
             while (rs.next()) {
+                //if the quantity of goods with this size is greater than zero
                 if (rs.getInt(LogPluCostEntry.COLUMN_QUANTITY) > 0) {
+                    log.finest("Quantity with this size > 0");
+                    //immediately go out with null
                     return null;
                 }
+                //if barcode matches argument...
                 if (rs.getString(BarcodeEntry.COLUMN_BARCODE).equals(barcode)) {
+                    // ... remember it in product
                     product = getProductFromResultSet(rs);
                 }
             }
         }catch (Exception e) {
-            log.log(Level.WARNING, "Exception: ", e.getMessage());
+            log.log(Level.WARNING, "Exception while getting DB query: ", e.getMessage());
+            // close all
             dispose();
         }
+
+        log.finest("Returning a new product: " + product);
+
         return product;
     }
 }
