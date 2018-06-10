2898efe6e65342ee158a94c279ded30ff15e2cc3
tv.unimol <doslovno@gmail.com>
2018-06-10 19:48:50 +0300

    Added docs to TableModel

--- src/main/java/dev/manifest/bccheck/table/TableModel.java
+++ src/main/java/dev/manifest/bccheck/table/TableModel.java
@@ -5,8 +5,11 @@ import dev.manifest.bccheck.util.MidiPlayer;
 import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class TableModel extends AbstractTableModel {
+
+    private final static Logger log = Logger.getLogger(TableModel.class.getName());
 
     private static TableModel instance;
     private TableModel(){
@@ -20,11 +23,19 @@ public class TableModel extends AbstractTableModel {
         return instance;
     }
 
+    /** total number of columns in the table. */
     private static final int COLUMN_COUNT = 4;
+    /** value of the "quantity" column for easy import to TradeX. */
     private static final int LAST_COLUMN_VALUE = 1;
 
+    /** List for storing table records. */
     private List<Product> productList = new ArrayList<>();
 
+    /**
+     * Returns the number of rows in the model. A JTable uses this method to determine how many rows it should display.
+     * This method should be quick, as it is called frequently during rendering.
+     * @return the number of rows in the model.
+     */
     @Override
     public int getRowCount() {
         return productList.size();
@@ -35,10 +46,16 @@ public class TableModel extends AbstractTableModel {
         return COLUMN_COUNT;
     }
 
+    /**
+     * Returns the value for the cell at columnIndex and rowIndex.
+     * @param rowIndex the row whose value is to be queried.
+     * @param columnIndex the column whose value is to be queried.
+     * @return the value Object at the specified cell.
+     */
     @Override
-    public Object getValueAt(int row, int column) {
-        Product product = productList.get(row);
-        switch (column) {
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Product product = productList.get(rowIndex);
+        switch (columnIndex) {
             case 0:
                 return product.getName();
             case 1:
@@ -52,6 +69,12 @@ public class TableModel extends AbstractTableModel {
         }
     }
 
+    /**
+     * Checks if TableModel contains product argument without taking it size
+     * @param product that we check for the presence in the TableModel
+     * @return false if product argument is null or TableModel does not contains this product
+     * or contains this article with any other size.
+     */
     public boolean containsArticle(Product product) {
         if (product == null) {
             return false;
@@ -62,12 +85,19 @@ public class TableModel extends AbstractTableModel {
             }
         }
         return false;
-
     }
 
+    /**
+     * Adds product to TableModel list, plays MIDI sound and notifies about rows changed.
+     * @param product that needs to be added.
+     */
     public void addProduct(Product product) {
+        // Play MIDI sound.
         MidiPlayer.playNewProduct();
         productList.add(product);
+        // Notify all listeners that all cell values in the table's rows may have changed.
         fireTableDataChanged();
+
+        log.info("Adding " + product + " to TableModel");
     }
 }
