ab8f908ba91f36620e51a9919756b6dd18fb60e1
tv.unimol <doslovno@gmail.com>
2018-06-10 18:18:03 +0300

    Added Logger to GlobalKeyListener

--- src/main/java/dev/manifest/bccheck/GlobalKeyListener.java
+++ src/main/java/dev/manifest/bccheck/GlobalKeyListener.java
@@ -1,14 +1,15 @@
 package dev.manifest.bccheck;
 
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
 public class GlobalKeyListener implements NativeKeyListener {
+
+    private final static Logger log = Logger.getLogger(GlobalKeyListener.class.getName());
 
     private OutputStream outputStream;
 
@@ -16,29 +17,29 @@ public class GlobalKeyListener implements NativeKeyListener {
         outputStream = out;
     }
 
+    /**
+     * Invoked when a key has been typed.
+     *
+     * @param keyEvent the native key event.
+     *
+     */
     @Override
     public void nativeKeyTyped(NativeKeyEvent keyEvent) {
 
     }
 
+    /**
+     * Invoked when a key has been pressed.
+     *
+     * @param keyEvent the native key event.
+     */
     @Override
     public void nativeKeyPressed(NativeKeyEvent keyEvent) {
-        //System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(keyEvent.getKeyCode()));
-
-        /*
-        if (keyEvent.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
-            try {
-                GlobalScreen.unregisterNativeHook();
-            } catch (NativeHookException e1) {
-                e1.printStackTrace();
-            }
-        }
-        */
         String keyString = NativeKeyEvent.getKeyText(keyEvent.getKeyCode());
         try {
             outputStream.write(keyString.getBytes());
         } catch (IOException e) {
-            e.printStackTrace();
+            log.severe("While writing to outputStream- " + keyString + " . Catch exception: " + e.getMessage());
         }
     }
 
